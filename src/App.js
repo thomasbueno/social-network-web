f2664181d063305a8ee5ea19e7eafe9f3630b156
Thomás Escolástico Bueno <62290004+thomasbueno@users.noreply.github.com>
2022-03-01 22:22:30 -0300

    creating initial config for login form

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/Login" element={<Login />} />
+          <Route path="/login/*" element={<Login />} />
         </Routes>
         <Footer />
       </BrowserRouter>
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Form from "./Form";
+import Register from "./Register";
+import ForgotPassword from "./ForgotPassword";
+import ResetPassword from "./ResetPassword";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Form />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot_password" element={<ForgotPassword />} />
+        <Route path="/reset_password" element={<ResetPassword />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default Login;
