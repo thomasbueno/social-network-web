e179d534d0e334bed3dea6f14768117d88cdf803
Thomás Escolástico Bueno <62290004+thomasbueno@users.noreply.github.com>
2022-03-04 21:10:26 -0300

    update form to use hook of useForm

--- src/Components/Login/Form.js
+++ src/Components/Login/Form.js
@@ -1,46 +1,40 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import useForm from "../../Hooks/useForm";
 import Button from "../Form/Button";
 import Input from "../Form/Input";
 
 function Form() {
-  const [username, setUsername] = React.useState("");
-  const [password, setPassword] = React.useState("");
+  const username = useForm();
+  const password = useForm();
 
   function handleSubmit(event) {
     event.preventDefault();
-    fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ username, password }),
-    })
-      .then((response) => {
-        console.log(response);
-        return response.json();
+
+    if (username.validation() && password.validation()) {
+      fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
       })
-      .then((json) => {
-        console.log(json);
-      });
+        .then((response) => {
+          console.log(response);
+          return response.json();
+        })
+        .then((json) => {
+          console.log(json);
+        });
+    }
   }
 
   return (
     <section>
       <h1>Login</h1>
       <form onSubmit={handleSubmit}>
-        <Input
-          name="username"
-          label="Login"
-          type="text"
-          onChange={({ target }) => setUsername(target.value)}
-        />
-        <Input
-          name="password"
-          label="Senha"
-          type="password"
-          onChange={({ target }) => setPassword(target.value)}
-        />
+        <Input name="username" label="Login" type="text" {...username} />
+        <Input name="password" label="Senha" type="password" {...password} />
         <Button>Entrar</Button>
       </form>
       <Link to="/login/register">Cadastrar</Link>
