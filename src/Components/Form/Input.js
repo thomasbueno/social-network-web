188604094a3c979a08270dde212526029d3fd80c
Thomás Escolástico Bueno <62290004+thomasbueno@users.noreply.github.com>
2022-03-04 21:09:57 -0300

    add new methods to input component

--- src/Components/Form/Input.js
+++ src/Components/Form/Input.js
@@ -1,14 +1,20 @@
 import React from "react";
 import styles from "./Input.module.css";
 
-const Input = ({ label, ...rest }) => {
+const Input = ({ label, value, onChange, error, onBlur, ...rest }) => {
   return (
     <div className={styles.wrapper}>
       <label className={styles.label} htmlFor="">
         {label}
       </label>
-      <input className={styles.input} {...rest} />
-      <p className={styles.error}>Error</p>
+      <input
+        className={styles.input}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+        {...rest}
+      />
+      {error && <p className={styles.error}>{error}</p>}
     </div>
   );
 };
